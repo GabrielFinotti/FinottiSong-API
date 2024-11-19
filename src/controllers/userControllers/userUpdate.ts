57786f70755cec75152a8181847714aaa8b4c509
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-11-18 23:51:45 -0300

    🔧 chore: Add function for updating user data

--- src/controllers/userControllers/userUpdate.ts
+++ src/controllers/userControllers/userUpdate.ts
@@ -1,8 +1,29 @@
 import { Request, Response } from "express";
+import { UserData } from "../../types/userDataType";
+import User from "../../models/User";
 
 const userUpdate = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const userData: Partial<UserData> = req.body;
+    const userId = parseInt(req.params.id);
+
+    const [updateRowsCount, [updatedUser]] = await User.update(userData, {
+      where: { id: userId },
+      returning: true,
+    });
+
+    if (updateRowsCount === 0) {
+      return res.status(404).send({ message: "No user found in the database" });
+    }
+
+    return res
+      .status(200)
+      .send([{ message: "Data successfully updated!" }, { updatedUser }]);
+  } catch (error) {
+    console.error(`Error updating user: ${error}`);
+
+    return res.status(500).send({ message: "Error updating user!" });
+  }
 };
 
 export default userUpdate;
